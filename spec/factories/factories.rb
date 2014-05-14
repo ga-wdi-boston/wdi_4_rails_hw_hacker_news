399d505fcde8876ab6b9f90e227a4b0c5aaa2256
Christine Lafontant <cklafontant@gmail.com>
2014-05-13 20:25:11 -0400

    User can view all articles on the homepage
      * added .day.ago for random date generation due to
        deprecation warning from rspec
      * added require 'faker' at top of factories file as fake data
        would not generate in rails console without it

---
+++ spec/factories/factories.rb
@@ -0,0 +1,8 @@
+require 'faker'
+FactoryGirl.define do
+  factory :article do
+    title { Faker::Internet.domain_name }
+    url { Faker::Internet.url }
+    created_at { rand(90).days.ago }
+  end
+end
