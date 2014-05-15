c9e90f2cdbe05c1cd3f8b67fabd67cad909bdd2c
Kathleen Votolato <kvotolato3@gmail.com>
2014-05-14 22:24:48 -0400

    Add Users to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,37 @@
+User.delete_all
+Article.delete_all
+
+# Create Users
+puts 'Creating users...'
+kathleen = User.create(email: "kvotolato3@gmail.com", password: "12345678")
+vilmos = User.create(email: "vilmos@mingspring.com", password: "csizmadia")
+joe = User.create(email: "joe@example.com", password: "12345678")
+kathy = User.create(email: "kathy@example.com", password: "12345678")
+
+puts 'Creating articles and comments'
 
 # Article 1 with 2 comments
 article = Article.create(title: "Passwords for JetBlue accounts cannot contain a Q or a Z", url: "http://www.jetblue.com")
-comment = Comment.create(body: "Wow, crazy stuff!")
+kathleen.articles << article
 
-article.comments << comment
+article.comments.create(body: "Wow, crazy stuff!", user: vilmos)
+article.comments.create(body: "Passwords are awesome!", user: joe)
 
-comment = Comment.create(body: "Passwords are awesome!")
-
-article.comments << comment
 
 # Article 2 with 2 comments
 
-article = Article.create(title: "Octotree: the missing GitHub tree view (Chrome extension)", url: "http://www.chrome.google.com")
-comment = Comment.create(body: "missing trees view")
+article = Article.create(title: "Octotree: the missing GitHub tree view (Chrome extension)", url: "http://chrome.google.com")
+kathy.articles << article
 
-article.comments << comment
+article.comments.create(body: "missing trees view", user: kathleen)
+article.comments.create(body: "I was wondering why that was happening", user: joe)
 
-comment = Comment.create(body: "I was wondering why that was happening")
 
-article.comments << comment
+# Article 3 with 2 comments
+
+article = Article.create(title: "Your Face and Name Will Appear in Google Ads Starting Today", url: "http://www.gizmodo.com")
+joe.articles << article
+
+article.comments.create(body: "your face...and your name too!", user: kathleen)
+article.comments.create(body: "today, what?", user: kathy)
+
