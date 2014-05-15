467a1d96c5cbb355e2478344c294b82ec72dfe27
Christine Lafontant <cklafontant@gmail.com>
2014-05-15 00:15:05 -0400

    User can add comment
      * created feature spec
      * updated comment controller (added new and create method)
      * updated index view
      * created new view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,4 +4,27 @@ class CommentsController < ApplicationController
     @comments = Comment.all
   end
 
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+
+    if @comment.save
+      flash[:notice] = 'Comment successfully added!'
+      redirect_to comments_path
+    else
+      flash.now[:alert] = @comment.errors.full_messages.join(', ')
+      render :new
+    end
+
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:title, :url)
+  end
+
 end
