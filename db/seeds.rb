c81ee4c671a942daf3fff71c0437def9b095b0cd
Amanda McDermott <amandapmcdermott@gmail.com>
2014-05-14 22:28:01 -0400

    Added seeds, Create Article migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Article.create!(
+  title: "So... I filmed strawberry growing (40 days in ~15 seconds)",
+  url: "http://i.imgur.com/ReboQ1W.gif")
+
+Article.create!(
+  title: "How to fix racism",
+  url: "http://i.imgur.com/btIZin9.jpg")
+
+Article.create!(
+  title: "Dropbox Webhooks",
+  url: "https://www.dropbox.com/developers/blog/90/announcing-dropbox-webhooks")
