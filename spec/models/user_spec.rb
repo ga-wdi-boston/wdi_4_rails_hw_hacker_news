8839fbc752beb907e495d240b1d6cc360d523e7a
Lisa Plesko <plesko.l@gmail.com>
2014-05-14 21:40:26 -0400

    Add additional validation for uniqueness of email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe User do
   it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email) }
   it { should validate_presence_of(:password) }
   it { should ensure_length_of(:password).is_at_least(8) }
   it { should_not allow_value("test@test").for(:email) }
