510c58d892b65b1103f2aefc5f68ec7f1d9f4511
Devin Dreszer <devindreszer@gmail.com>
2014-06-30 18:18:04 -0400

    App has seed data for a user and some articles
    * Users have many articles and comments
    * Articles have many comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ActiveRecord::Base
-  validates :body, presence: true
-
   belongs_to :article
   belongs_to :user
+
+  validates :body, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.delete_all
+Article.delete_all
+
+devin = User.create!(
+  email: "devin@example.com",
+  password: "password"
+)
+
+devin.articles.create!(
+  title: "Ruby on Rails Guides",
+  url: "http://guides.rubyonrails.org/"
+)
+
+devin.articles.create!(
+  title: "Rails Documentation",
+  url: "http://api.rubyonrails.org/"
+)
+
+devin.articles.create!(
+  title: "CAP Theorem: Explained",
+  url: "http://robertgreiner.com/2014/06/cap-theorem-explained/"
+)
+
+devin.articles.create!(
+  title: "An Introduction To Full-Stack JavaScript",
+  url: "http://www.smashingmagazine.com/2013/11/21/introduction-to-full-stack-javascript/"
+)
+
+devin.articles.create!(
+  title: "Angular, Ember, And Backbone: Which JavaScript Framework Is Right For You?",
+  url: "http://readwrite.com/2014/02/06/angular-backbone-ember-best-javascript-framework-for-you#awesm=~oIlDDgJzjmlbSG"
+)
