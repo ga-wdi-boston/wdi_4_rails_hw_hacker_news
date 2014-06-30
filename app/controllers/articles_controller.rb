c975d011d7f8d8468f31e84206a5a137c125c346
brimatteng <btenggren20@gmail.com>
2014-06-30 17:29:46 -0400

    Add Article model, controller, db, and routes (all empty)

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,30 @@
+class ArticlesController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  #new and create!
+  def new
+  end
+
+  def create
+  end
+
+  #edit and update!
+  def edit
+  end
+
+  def update
+  end
+
+  #delete and destroy!
+  def delete
+  end
+
+  def destroy
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
 
   devise_for :users
+
+  resources :articles
+
+  root 'articles#index'
 end
