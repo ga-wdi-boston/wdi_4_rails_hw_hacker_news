743edbadf132ff4ba5ff929e48b89f11e093c20b
Emma Gardner <emmagardner@outlook.com>
2014-06-30 17:54:37 -0400

    added associations for user, articles, and comments.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ActiveRecord::Base
   validates :title, :url, presence: true
 
+  belongs_to :user
+  has_many :comments
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ActiveRecord::Base
   validates :body, presence: true
+
+  belongs_to :user
+  belongs_to :article
 end
