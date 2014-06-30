c0ddcb8d7dd9568d9666498ed3a59b6b81d4f2a7
Ben White <whtouche@gmail.com>
2014-06-30 17:16:03 -0400

    Create Articles table

---
+++ db/migrate/20140630211159_create_articles.rb
@@ -0,0 +1,10 @@
+class CreateArticles < ActiveRecord::Migration
+  def change
+    create_table :articles do |t|
+      t.text :title, null: false
+      t.text :url, null: false
+
+      t.timestamps
+    end
+  end
+end
