a532d0c7c11a05288869acd929bbd87fa4d7e0c0
Emma Gardner <emmagardner@outlook.com>
2014-06-30 21:44:25 -0400

    made migration to add belongs_to :user to articles

---
+++ db/migrate/20140701013347_add_belongs_to_to_article.rb
@@ -0,0 +1,8 @@
+class AddBelongsToToArticle < ActiveRecord::Migration
+  def change
+    change_table :articles do |t|
+      t.belongs_to :user
+    end
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140630213645) do
+ActiveRecord::Schema.define(version: 20140701013347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20140630213645) do
     t.string   "url",        null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "comments", force: true do |t|
