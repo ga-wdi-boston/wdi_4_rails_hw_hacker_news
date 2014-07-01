ee81df7df0234029b8738d24d860bca66c9900da
Jodi Detch <jodidetch@gmail.com>
2014-06-30 22:32:29 -0400

    Users can view a list of all articles with working seed data

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
 
   devise_for :users
+
+  resources :articles, only: [:index, :show]
+
+  root 'articles#index'
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Article.delete_all
+
+article1 = Article.create(title: 'title1', url: 'www.test1.com', submitted_at: '01-01-2001')
+article2 = Article.create(title: 'title2', url: 'www.test2.com', submitted_at: '01-02-2001')
+article3 = Article.create(title: 'title3', url: 'www.test3.com', submitted_at: '01-03-2001')
+article4 = Article.create(title: 'title4', url: 'www.test4.com', submitted_at: '01-04-2001')
+article5 = Article.create(title: 'title5', url: 'www.test5.com', submitted_at: '01-05-2001')
