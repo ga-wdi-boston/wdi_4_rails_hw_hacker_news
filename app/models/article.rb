7c39e2eb0ad1f4783741df4c9ba527ce94bc2d79
brimatteng <btenggren20@gmail.com>
2014-07-01 17:58:14 -0400

    Add's parent/child/poly references between models and create comment model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ActiveRecord::Base
   belongs_to :user
-  has_many :comments, dependent: :destroy
+  has_many :comments, as: :commentable, dependent: :destroy
 
   validates :title, presence:true
   validates :url, presence:true
