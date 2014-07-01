c9f4b49b23abe3e3bfeaa4b311fe1370595da8b9
Devin Dreszer <devindreszer@gmail.com>
2014-07-01 07:30:07 -0400

    Signed-in users can submit articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,29 @@
 class ArticlesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
 
   def index
     @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Posted #{@article.title}"
+      redirect_to articles_path
+    else
+      flash.now[:alert] = @article.errors.full_messages.join(', ')
+      render :new
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :url)
   end
 
 end
