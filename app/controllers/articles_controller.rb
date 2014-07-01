c57210e05fac9da3aa13c7c13e78641c37dda720
Blair Caple <blair@kaaterskil.com>
2014-07-01 05:45:52 -0400

    build article list views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit]
   before_action :find_article, except: [:index, :new, :create]
 
   # GET /
--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,26 @@ class Article < ActiveRecord::Base
   belongs_to :user
   has_many :comments, dependent: :destroy
 
+  def host
+    URI(@url).host
+  end
+
+  def list_score
+    if comments.empty?
+      '0 points'
+    else
+      'TODO!!!'
+    end
+  end
+
+  def list_comment_count
+    if comments.empty?
+      'discuss'
+    else
+      "#{comments.size} comments"
+    end
+  end
+
   validates :title, presence: true
   validates_format_of :url, with: URI::regexp(%w(http https))
 end
