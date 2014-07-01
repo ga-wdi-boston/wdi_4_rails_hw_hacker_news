fe5363ba83d15ba46f791c2d1560a9cbaee4b031
zhbanton <zhbanton@gmail.com>
2014-06-30 21:52:42 -0400

    Articles indexed on homepage

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,12 @@
 class ArticlesController < ApplicationController
 
   def index
+    @articles = Article.all
   end
 
   def show
+    @article = Article.find(params[:id])
+    @comments = @article.comments
   end
 
 end
