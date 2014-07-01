a87254e3c6736e2eb7d6e13211efd1699ced54cf
Emma Gardner <emmagardner@outlook.com>
2014-07-01 09:21:12 -0400

    added dependent: : destroy to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :articles
-  has_many :comments
+  has_many :articles, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
 
 end
