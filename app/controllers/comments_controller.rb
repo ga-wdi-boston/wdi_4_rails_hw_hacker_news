d10b3568c519699256cf7782e1d481a81c4faf79
Devin Dreszer <devindreszer@gmail.com>
2014-07-01 09:49:11 -0400

    Signed-in users can leave comments on articles

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,21 @@ class CommentsController < ApplicationController
     @comments = @article.comments
   end
 
+  def new
+    @comment = @article.comments.new
+  end
+
+  def create
+    @comment = @article.comments.new(comments_params)
+    if @comment.save
+      flash[:notice] = "Posted #{@comment.title}"
+      redirect_to @comments
+    else
+      flash.now[:alert] = @comment.errors.full_messages.join(', ')
+      render :new
+    end
+  end
+
   private
 
   def set_article
