b1cd0857c3755ad8a11a7842d1406532ad24efd0
Julie <juliesof@gmail.com>
2014-07-01 20:40:57 -0400

    Article Controller with functioning CRUD methods

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,33 @@
+class ArticlesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
+  def index
+    @article = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+    @comment = Comment.new
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = current_user.articles.new(article_params)
+
+    if @article.save
+      redirect_to @article, notice: 'article was successfully created.'
+    else
+      flash.now[:alert] = @article.errors.full_messages.join(', ')
+      render :new
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:url, :title)
+  end
+end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ActiveRecord::Base
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
-  has_many :likes, as: :likeable, dependent: :destroy
+  #has_many :likes, as: :likeable, dependent: :destroy
   validates :url, :title, presence: true
   validates :url, format: { with: /\Ahttp/ }
 end
