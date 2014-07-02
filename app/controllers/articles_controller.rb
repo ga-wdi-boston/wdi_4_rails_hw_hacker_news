3c4a487b77200cd37fea85a8b836bd5772bbeb87
Emma Gardner <emmagardner@outlook.com>
2014-07-02 18:15:12 -0400

    made commentcontrollers index new and create actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,9 +3,7 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
-  def show
-    @article = Article.find(params[:id])
-  end
+#don't need the show page
 
   def new
     @article = Article.new
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,8 +4,32 @@ class CommentsController < ApplicationController
     @comments = @article.comments
   end
 
-  def new #NOPE
+  def new
     @comment = Comment.new
     @article = Article.find(params[:article_id])
   end
+
+  def create
+
+    @article = Article.find(params[:article_id])
+    #this code does this:
+    #@comment = Comment.new
+    #@comment.body = params[:body]
+    #@comment.article = @article
+    # pushes new comment into article.comments
+    @comment = @article.comments.new(comment_params)
+    if @comment.save
+      redirect_to article_comments_path(@article)
+    else
+      render :new
+    end
+
+  end
+ private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+
+  def
 end
