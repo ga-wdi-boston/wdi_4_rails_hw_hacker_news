5dc50525990ae6d1523ba852720c336c245e8c5c
Jake Austin <jakeaust07@gmail.com>
2014-07-02 09:36:27 -0400

    seed data for users, articles, comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Users
+user1 = User.create!(email: 'jake@example.com', password: 'password')
+user2 = User.create!(email: 'steve@example.com', password: 'password')
+user3 = User.create!(email: 'mark@example.com', password: 'password')
+user4 = User.create!(email: 'tom@example.com', password: 'password')
+user5 = User.create!(email: 'alex@example.com', password: 'password')
+
+# Articles
+user1.articles.create!(url: "http://www.google.com", title: "Awesome search engine")
+user2.articles.create!(url: "http://www.reddit.com/r/ruby", title: "Ruby Subreddit")
+user4.articles.create!(url: "http://www.hackernews.com", title: "The OG hacker news")
+
+# Comments
+Comment.create!(body: "Duh, it's google", user_id: 3, article_id: 1)
+Comment.create!(body: "le repost", user_id: 4, article_id: 2)
+Comment.create!(body: "clearly the superior site", user_id: 2, article_id: 3)
+Comment.create!(body: "I like this place more", user_id: 1, article_id: 3)
+
+
+# Votes
