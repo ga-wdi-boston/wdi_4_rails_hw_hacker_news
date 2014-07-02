33cd30764fe53fb2cff30bae2754c8be32510309
Blair Caple <blair@kaaterskil.com>
2014-07-01 21:07:50 -0400

    initial routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :articles do
-    resources :comments
+    resources :comments, except: [:index, :destroy]
+    resources :votes, except: :index
+  end
+
+  resources :comments, only: :show do
+    resources :votes, except: :index
   end
 
   root 'articles#index'
