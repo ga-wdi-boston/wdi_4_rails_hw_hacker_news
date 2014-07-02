f4ae730cc77d6e78d760d23942a2b098ecc55f01
Max Kohl <maxkohl810@gmail.com>
2014-07-02 13:46:51 -0400

    Users can view a list of all articles and submit their own

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,24 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = current_user.articles.new(article_params)
+
+    if @article.save
+      redirect_to root_path, notice: 'You successfully created an article.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :url)
+  end
 end
