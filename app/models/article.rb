f774321631bfb44f8edd05c657a356f7aec27d8c
Devin Dreszer <devindreszer@gmail.com>
2014-07-02 11:31:38 -0400

    Articles and comments have scores

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,4 +6,12 @@ class Article < ActiveRecord::Base
   validates :title, presence: true
   validates :url, format: { with: /\Ahttp/ }
   # validates! :user_id, presence: true
+
+  def score
+    score = 0
+    self.votes.each do |vote|
+      score += vote.is_up ? +1 : -1
+    end
+    score
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,4 +5,12 @@ class Comment < ActiveRecord::Base
 
   validates :body, presence: true
   # validates! :user_id, :article_id, presence: true
+
+  def score
+    score = 0
+    self.votes.each do |vote|
+      score += vote.is_up ? +1 : -1
+    end
+    score
+  end
 end
