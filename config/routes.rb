1131c7969fa9e75044c514dd9b4f63a985ce9383
testkitschen <katryn.mcintosh@gmail.com>
2014-07-02 23:41:20 -0400

    create empty files for comments controller, downvotes controller, upvotes controller and add before authenticate line to articles controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,11 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-
 resources :articles do
     resources :comments, only: [:new, :create]
     resources :upvotes, only: [:create, :destroy]
     resources :downvotes, only: [:create, :destroy]
   end
-
-
-
-
-
-
 
   root 'articles#index'
 
