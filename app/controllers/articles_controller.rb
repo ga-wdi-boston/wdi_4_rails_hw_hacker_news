1b4568513357427cf56eafb7ffe1da9ea31b8e74
testkitschen <katryn.mcintosh@gmail.com>
2014-07-02 23:44:27 -0400

    Add a show method to the articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,11 @@ class ArticlesController < ApplicationController
 
   def index
      @articles = Article.order(created_at: :desc)
+  end
 
+  def show
+    @article = Article.find(params[:id])
+    @comment = Comment.new
   end
 
 end
