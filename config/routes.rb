dd8d1169b7201b99a2e2a62d8b86c17f5f9737a2
testkitschen <katryn.mcintosh@gmail.com>
2014-07-02 23:34:44 -0400

    Add resources to route file and rake routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,20 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  resources :articles
+
+
+resources :articles do
+    resources :comments, only: [:new, :create]
+    resources :upvotes, only: [:create, :destroy]
+    resources :downvotes, only: [:create, :destroy]
+  end
+
+
+
+
+
+
+
   root 'articles#index'
 
 end
