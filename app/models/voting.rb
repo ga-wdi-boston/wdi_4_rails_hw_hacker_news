d67cfff71940ab79692c4d185181b4384ffc7601
Blair Caple <blair@kaaterskil.com>
2014-07-03 09:12:31 -0400

    add weighted vote feature

--- app/models/voting.rb
+++ app/models/voting.rb
@@ -1,14 +1,25 @@
 module Voting
 
   def points
-    upvotes - downvotes
+    weighted_votes(upvotes) - weighted_votes(downvotes)
   end
 
   def upvotes
-    self.votes.where(vote: 'upvote').count
+    self.votes.where(vote: 'upvote')
   end
 
   def downvotes
-    self.votes.where(vote: 'downvote').count
+    self.votes.where(vote: 'downvote')
+  end
+
+  private
+
+  def weighted_votes(votes)
+    now = Time.now.getutc
+    result = 0
+    votes.each do |vote|
+      result += 1 ** ( 1 - (vote.created_at.to_time.utc.to_f / now.to_f))
+    end
+    result.round
   end
 end
