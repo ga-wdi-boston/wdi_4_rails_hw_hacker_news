ebaec198462b245a96a411953614010f3c6eee66
brimatteng <btenggren20@gmail.com>
2014-07-03 08:47:34 -0400

    Add votes as voteable and references in each model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ActiveRecord::Base
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
+  has_many :votes, as: :voteable, dependent: :destroy
 
   validates :title, presence:true
   validates :url, presence:true
