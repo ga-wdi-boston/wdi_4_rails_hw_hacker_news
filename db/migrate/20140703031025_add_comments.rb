4c7595aac48d54a91e71e0de32463eb57abd1d0a
testkitschen <katryn.mcintosh@gmail.com>
2014-07-02 23:15:19 -0400

    Create comments migration

---
+++ db/migrate/20140703031025_add_comments.rb
@@ -0,0 +1,11 @@
+class AddComments < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+      t.text :content, null: false
+      t.references :user, index: true
+      t.references :article, index: true
+
+      t.timestamps
+    end
+  end
+end
